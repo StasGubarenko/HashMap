8292136854dad514382cebff60171f5224f1f4e2
StasGubarenko <120662529+StasGubarenko@users.noreply.github.com>
2023-04-07 21:10:31 +0600

    Update HashMapTest.java

--- HashMapTest.java
+++ HashMapTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 
 class MyHashMapTest {
     @Test
@@ -175,6 +177,99 @@ class MyHashMapTest {
         }
     }
 
+    @Test
+    @DisplayName("Проверка remove() при пустой мапе")
+    public void removeWhenHashMapIsEmpty(){
+        MyHashMap<Integer, Integer> hashMap = new MyHashMap<>();
+        Assertions.assertNull(hashMap.remove(33));
+    }
+
+    @Test
+    @DisplayName("Проверка remove() при размере мапы 16(нет коллизии)")
+    public void removeWhenHashMapHasSize16(){
+        MyHashMap<Integer, Integer> hashMap = new MyHashMap<>();
+        Integer value = 10;
+
+        for(int i = 0; i < hashMap.getDefaultSize();i++){
+            hashMap.put(i,value);
+            value += 10;
+        }
+
+        Integer[] arraysOfKeys = new Integer[hashMap.getDefaultSize()];
+
+        Integer value2 = 10;
+        for(int i = 0; i < hashMap.getDefaultSize(); i++){
+            arraysOfKeys[i] = value2;
+            value2 += 10;
+        }
+
+        arraysOfKeys[2] = null;
+
+        Assertions.assertEquals(30,hashMap.remove(2));
+
+        Assertions.assertAll(() ->{
+            for(int i = 0; i < arraysOfKeys.length; i++){
+                if(!Objects.equals(arraysOfKeys[i], hashMap.get(i))){
+                    throw new RuntimeException();
+                }
+            }
+        });
+
+        Assertions.assertEquals(15,hashMap.size());
+    }
+
+    @Test
+    @DisplayName("Проверка удаления ноды из массива при наличии коллизии")
+    public void removeNodeFromArrayWhenThereIsCollision(){
+        MyHashMap<Integer, Integer> hashMap = new MyHashMap<>();
+        Integer value = 10;
+
+        for(int i = 0; i < 100;i++){
+            hashMap.put(i,value);
+            value += 10;
+        }
+
+        Integer[] arraysOfKeys = new Integer[100];
+
+        Integer value2 = 10;
+        for(int i = 0; i < 100; i++){
+            arraysOfKeys[i] = value2;
+            value2 += 10;
+        }
+        arraysOfKeys[0] = 170;
+
+        Assertions.assertEquals(10,hashMap.remove(0));
+        Assertions.assertEquals(99,hashMap.size());
+
+        hashMap.remove(0);
+
+    }
+
+    //CСпросить как проверить
+    @Test
+    @DisplayName("Удаление ноды из связанного списка в мапе")
+    public void removeNodeInLinkedListInHashMap(){
+        MyHashMap<Integer, Integer> hashMap = new MyHashMap<>();
+        Integer value = 10;
+
+        for(int i = 0; i < 100;i++){
+            hashMap.put(i,value);
+            value += 10;
+        }
+
+        Integer[] arraysOfKeys = new Integer[100];
+
+        Integer value2 = 10;
+        for(int i = 0; i < 100; i++){
+            arraysOfKeys[i] = value2;
+            value2 += 10;
+        }
+
+
+        Assertions.assertEquals(650,hashMap.remove(64));
+        Assertions.assertEquals(99,hashMap.size());
+
+    }
 
 
 }
